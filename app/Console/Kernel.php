eb8ebdff8f03921ff000c404408a0088c179be9c
materialsmoke <101429685+materialsmoke@users.noreply.github.com>
2022-08-31 20:12:18 +0200

    First

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -35,7 +35,7 @@ class Kernel extends ConsoleKernel
                 ]);
                 SendRequestJob::dispatch($domain, $curlDetail);
             }
-        })->everyTwoMinutes();
+        })->everyMinute();
     }
 
     /**
