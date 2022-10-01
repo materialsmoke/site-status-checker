5d00ea82f15cfa44d0e91d6e563a973dbc8c684a
materialsmoke <101429685+materialsmoke@users.noreply.github.com>
2022-10-02 00:40:41 +0200

    import carbon

--- app/Jobs/SendRequestJob.php
+++ app/Jobs/SendRequestJob.php
@@ -2,15 +2,16 @@
 
 namespace App\Jobs;
 
+use Carbon\Carbon;
 use App\Models\CurlDetail;
 use Illuminate\Bus\Queueable;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
-use Illuminate\Support\Facades\Log;
 
 class SendRequestJob implements ShouldQueue
 {
